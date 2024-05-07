c598a41ae9cf43cdc837f7f8146e61fb9a8f90af
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-07 23:14:09 +0300

    error fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
+
 def pascal_triangle(n):
     """prints a pascals triangle"""
 
     answer = [[1]]
     if n <= 0:
         return []
-    for i in range(n -1):
+    for i in range(n - 1):
         row = [1]
-        row.extend([answer[i][j] + answer[i][j + 1] for j in range(len(answer[i]) - 1)])
+        row.extend([answer[i][j] + answer[i][j + 1]
+                    for j in range(len(answer[i]) - 1)])
         row.append(1)
         answer.append(row)
     return answer
